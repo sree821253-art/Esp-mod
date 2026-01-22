8e8a03ea7df32a512c97c8a3f6a8b7b998402a39
sree821253-art <sree821253@gmail.com>
2026-01-22 20:21:44 +0530

    Update device.dart

--- lib/models/device.dart
+++ lib/models/device.dart
@@ -70,7 +70,7 @@ class Device {
   int? gpioPin;
 int? statusGpioPin; // ADD THIS LINE - for reading actual switch state
 String? roomId;
-  ol isParent; // ADD THIS - ESP32 parent or ESP8266 child
+  bool isParent; // ADD THIS - ESP32 parent or ESP8266 child
 String? parentId; // ADD THIS - Reference to parent device
 String? staticIP;
   bool isOn;
